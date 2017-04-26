b377fffbc5d6e2090d7fb7cbaa44bef800cf4079
Karthik Sreekumar <kskarthiksk@gmail.com>
2017-04-26 10:08:58 +0000

    Implemented binary search in helpers.c

--- find/helpers.c
+++ find/helpers.c
@@ -13,7 +13,20 @@
  */
 bool search(int value, int values[], int n)
 {
-    // TODO: implement a searching algorithm
+    //Binary search
+   int L=0,U=n,mid;
+    if(n<=0)
+        return false;
+    while(L<=U)
+    {
+        mid=(L+U)/2;
+        if(value==values[mid])
+            return true;
+        else if(value<values[mid])
+            U=mid-1;
+        else
+            L=mid+1;
+    }
     return false;
 }
 
